cdf6ca4ae9aa34c5b1589afe56219f65d21e1fe3
Jack Hubert <jphubert93@gmail.com>
2021-03-09 15:05:43 -0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
 
+import { Redirect, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
+      <Route
+        exact
+        path="/"
+        render={(routerProps) => <Home {...routerProps} name="Jack" /> } />
+      <Route path="/about" >
+        <About />
+      </Route>
+      <Route path="/contact" component={Contact} />
+
+      <PrivateRoute path="/profile" component={About}/>
     </div>
   );
 }
+
+function PrivateRoute({ component: Component , ...props }){
+  return localStorage.token
+  ? <Route  {...props} render={(routerProps) => <Component {...routerProps} {...props} />}/>
+  : <Redirect to='/login' />
+}
+
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 export default function Home(props) {
+
+  const handleClick = () => {
+    props.history.push('/about')
+  }
+
   return (
     <div>
       <h1>Home Page</h1>
+      <button onClick={handleClick}>Go to About</button>
     </div>
   )
 }
